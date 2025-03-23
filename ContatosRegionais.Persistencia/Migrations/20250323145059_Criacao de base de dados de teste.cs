073a17e6928b98633a9b171240152c8ace384972
dutra-rafael <rafael.dutra0001@gmail.com>
2025-03-23 11:53:10 -0300

    Inclusão de base de dados de teste

---
+++ ContatosRegionais.Persistencia/Migrations/20250323145059_Criacao de base de dados de teste.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContatosRegionais.Persistencia.Migrations
+{
+    /// <inheritdoc />
+    public partial class Criacaodebasededadosdeteste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
